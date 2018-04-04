23713741d29401a256bba5cb753726b4f2a691da
DESKTOP-FSO6OT6\infol <topinfolga@gmail.com>
2018-04-04 20:11:49 +0300

    промежуточное 3.03 -синхронизация версий клиента и сервера -реализована создание чата с найденным пользователем - отображение бесед (почти)

--- src/main/java/ServerTestInfolga.java
+++ src/main/java/ServerTestInfolga.java
@@ -6,66 +6,74 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.regex.Pattern;
 
 public class ServerTestInfolga {
     public static Connection c;
     public static Statement st;
 
     public static void main(String[] args) throws Exception {
-
-        boolean matches = Pattern.matches("[+]?[38]?\\d{10}", "+38045646");
         try {
 
+            String url;
+            String user;
+            String password;
+            PoolingDB DB;
+            Server_Tetst ST;
+            QueueTask QT = new QueueTask();
+
+            FileReader file = new FileReader("Seting.txt");
+            BufferedReader reader = new BufferedReader(file);
+            url = reader.readLine();
+            user = reader.readLine();
+            password = reader.readLine();
+            file.close();
+            System.out.println(url);
+            System.out.println(user);
+            System.out.println("psaa " + password);
 
 
+            c = DriverManager.getConnection(url,user,password);
+            st = c.createStatement();
 
-        String url;
-        String user;
-        String password;
-        PoolingDB DB;
-        Server_Tetst ST;
-        QueueTask QT = new QueueTask();
+            ResultSet rs = st.executeQuery("SHOW DATABASES;");
 
-        FileReader file = new FileReader("Seting.txt");
-        BufferedReader reader = new BufferedReader(file);
-        url = reader.readLine();
-        user = reader.readLine();
-        password = reader.readLine();
-        file.close();
-        System.out.println(url);
-        System.out.println(user);
-        System.out.println("psaa " + password);
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
 
-        DB = new PoolingDB(url, user, password);
+            rs.close();
+            st.close();
+            c.close();
 
-        String PORT = MyProperties.instans().getProperty("port", "8080");
-        int port = Integer.parseInt(PORT);
-        ST = new Server_Tetst(QT);
+            DB = new PoolingDB(url, user, password);
 
-        QT.start(ST, DB, port);
-        System.out.println("redy");
-        QT.join();
+            String PORT = MyProperties.instans().getProperty("port", "8080");
+            int port = Integer.parseInt(PORT);
+            ST = new Server_Tetst(QT);
+
+            QT.start(ST, DB, port);
+            System.out.println("redy");
+            QT.join();
 
 
-    } catch(
-    FileNotFoundException e)
+        } catch (
+            FileNotFoundException e)
 
-    {
-        e.printStackTrace();
-    } catch(
-    IOException e)
+        {
+            e.printStackTrace();
+        } catch (
+            IOException e)
 
-    {
-        e.printStackTrace();
-    } catch(
-    InterruptedException e)
+        {
+            e.printStackTrace();
+        } catch (
+            InterruptedException e)
 
-    {
-        e.printStackTrace();
+        {
+            e.printStackTrace();
+        }
+
     }
-
-}
 }
 
 
